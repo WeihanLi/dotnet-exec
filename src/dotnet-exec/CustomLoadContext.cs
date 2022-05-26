2f04e4a1c16bcb9e1964ac118d48bb5abecf6fd8
Weihan Li <weihanli@outlook.com>
2022-05-26 23:14:36 +0800

    Add CustomLoadContext

---
+++ src/dotnet-exec/CustomLoadContext.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Exec;
+
+// https://docs.microsoft.com/en-us/dotnet/core/tutorials/creating-app-with-plugin-support
+public sealed class CustomLoadContext : AssemblyLoadContext
+{
+    private readonly Dictionary<string, string> _assemblyPaths;
+
+    public CustomLoadContext(string[] assemblyPaths)
+    {
+        _assemblyPaths = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        foreach (var assemblyPath in assemblyPaths)
+        {
+            // Add the first entry with the same simple assembly name if there are multiples
+            // and ignore others
+            _assemblyPaths.TryAdd(Path.GetFileNameWithoutExtension(assemblyPath), assemblyPath);
+        }
+    }
+
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        if (assemblyName.Name != null 
+            && _assemblyPaths.TryGetValue(assemblyName.Name, out var assemblyPath))
+        {
+            return LoadFromAssemblyPath(assemblyPath);
+        }
+        return null;
+    }
+}
