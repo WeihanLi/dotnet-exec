0b5099d420cd152f9b04991680ee37620becb9e7
Weihan Li <weihanli@outlook.com>
2022-06-14 12:07:18 +0800

    Remove System.CommandLine.Hosting

--- src/dotnet-exec/Program.cs
+++ src/dotnet-exec/Program.cs
@@ -1,29 +1,11 @@
 ﻿// Copyright (c) Weihan Li. All rights reserved.
 // Licensed under the MIT license.
 
-using System.CommandLine.Builder;
-using System.CommandLine.Hosting;
 using System.CommandLine.Invocation;
 
+await using var serviceProvider = new ServiceCollection()
+    .RegisterApplicationServices(args)
+    .BuildServiceProvider();
 var command = ExecOptions.GetCommand();
-await new CommandLineBuilder(command)
-    .UseDefaults()
-    .UseHost(hostBuilder =>
-    {
-        hostBuilder.ConfigureServices((_, services) =>
-        {
-            services.RegisterApplicationServices(args);
-        });
-    })
-    .AddMiddleware(invocationContext =>
-    {
-        var serviceProvider = invocationContext.BindingContext.GetService<IHost>()?.Services
-                              ?? invocationContext.BindingContext;
-        var commandHandler = serviceProvider.GetService<ICommandHandler>();
-        if (command.Handler is null && commandHandler != null)
-        {
-            command.Handler = commandHandler;
-        }
-    })
-    .Build()
-    .InvokeAsync(args);
+command.Handler = serviceProvider.GetRequiredService<ICommandHandler>();
+await command.InvokeAsync(args);
