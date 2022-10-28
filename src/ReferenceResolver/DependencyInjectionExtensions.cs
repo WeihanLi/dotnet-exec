f526c9a64a3ca18a277f9f55b4fe6495f57e3472
Weihan Li <weihanli@outlook.com>
2022-10-28 22:54:00 +0800

    Add DependencyInjectionExtensions for ReferenceResolver

---
+++ src/ReferenceResolver/DependencyInjectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ReferenceResolver;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddReferenceResolvers(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddLogging();
+        serviceCollection.TryAddSingleton<IReferenceResolver, FileReferenceResolver>();
+        serviceCollection.TryAddSingleton<IReferenceResolver, FolderReferenceResolver>();
+        serviceCollection.TryAddSingleton<IReferenceResolver, FrameworkReferenceResolver>();
+        serviceCollection.TryAddSingleton<IReferenceResolver, NuGetReferenceResolver>();
+        serviceCollection.TryAddSingleton<IReferenceResolverFactory, ReferenceResolverFactory>();
+        return serviceCollection;
+    }
+}
