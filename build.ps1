6d8395d6b90f7c9c3c7e998d4372e68e2c13a87c
Weihan Li <weihanli@outlook.com>
2022-11-20 23:46:00 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
-[string]$SCRIPT       = '.\build.cake'
- 
-# Install cake.tool
-dotnet tool install --global cake.tool
+[string]$SCRIPT = '.\build.cake'
+
+# set environment variables
+[System.Environment]::SetEnvironmentVariable('CI', 'true')
+
+# Install the latest cake.tool
+dotnet tool update --global cake.tool
 
 # Start Cake
 Write-Host "dotnet cake $SCRIPT $ARGS" -ForegroundColor GREEN
- 
+
+# Execute Cake
 dotnet cake $SCRIPT $ARGS
