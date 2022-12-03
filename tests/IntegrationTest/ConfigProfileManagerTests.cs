cb42d4b6bcb33bd099a24081ed9df7b5b89a79bd
Weihan Li <weihanli@outlook.com>
2022-12-03 14:06:19 +0800

    Add ConfigProfileManagerTests

---
+++ tests/IntegrationTest/ConfigProfileManagerTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Weihan Li. All rights reserved.
+// Licensed under the MIT license.
+
+namespace IntegrationTest;
+
+public class ConfigProfileManagerTests
+{
+    private readonly IConfigProfileManager _configProfileManager;
+
+    public ConfigProfileManagerTests(IConfigProfileManager configProfileManager)
+    {
+        _configProfileManager = configProfileManager;
+    }
+
+    [Fact]
+    public async Task ProfileOperation()
+    {
+        var profile = new ConfigProfile()
+        {
+            IncludeWideReferences = false,
+            EntryPoint = "Execute"
+        };
+        var name = Guid.NewGuid().ToString();
+        var getProfile = await _configProfileManager.GetProfile(name);
+        Assert.Null(getProfile);
+        try
+        {
+            await _configProfileManager.ConfigureProfile(name, profile);
+            getProfile = await _configProfileManager.GetProfile(name);
+            Assert.NotNull(getProfile);
+            Assert.Equal(profile.IncludeWideReferences, getProfile.IncludeWideReferences);
+            Assert.Equal(profile.EntryPoint, getProfile.EntryPoint);
+
+            var profiles = await _configProfileManager.ListProfiles();
+            Assert.NotEmpty(profiles);
+        }
+        finally
+        {
+            await _configProfileManager.DeleteProfile(name);
+        }
+    }
+}
