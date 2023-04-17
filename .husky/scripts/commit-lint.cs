98f5dd5ee8a157d7f9e9f96a1aa91b115940a111
Weihan Li <weihanli@outlook.com>
2023-04-17 21:09:23 +0800

    feat: use dotnet-exec for commit lint

--- .husky/scripts/commit-lint.cs
+++ .husky/scripts/commit-lint.cs
@@ -1,12 +1,12 @@
-/// <summary>
-/// a simple regex commit linter example
-/// https://www.conventionalcommits.org/en/v1.0.0/
-/// https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
-/// </summary>
+/*
+A simple regex commit linter
+https://www.conventionalcommits.org/en/v1.0.0/
+https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type
+*/
 
 const string pattern = @"^(?=.{1,90}$)(?:build|feat|ci|chore|docs|fix|perf|refactor|revert|style|test)(?:\(.+\))*(?::).{4,}(?:#\d+)*(?<![\.\s])$";
 
-var msg = File.ReadAllLines(Args[0])[0];
+var msg = File.ReadAllLines(args[0])[0];
 Console.WriteLine("Your commit headline message is:\n> {0}", msg);
 if (System.Text.RegularExpressions.Regex.IsMatch(msg, pattern))
    return 0;
