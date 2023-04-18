48eef3a7b013b69fda2a44a6789ec8bad638c42d
Weihan Li <weihanli@outlook.com>
2023-04-18 23:48:48 +0800

    chore: update license header

--- src/dotnet-exec/Program.cs
+++ src/dotnet-exec/Program.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Weihan Li. All rights reserved.
-// Licensed under the MIT license.
+﻿// Copyright (c) 2022-2023 Weihan Li. All rights reserved.
+// Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
 await using var serviceProvider = new ServiceCollection()
     .RegisterApplicationServices(args)
--- tests/IntegrationTest/CodeSamples/EmbeddedReferenceSample.cs
+++ tests/IntegrationTest/CodeSamples/EmbeddedReferenceSample.cs
@@ -1,4 +1,7 @@
-﻿// reference: nuget:WeihanLi.Npoi, 2.4.2
+﻿// Copyright (c) 2022-2023 Weihan Li. All rights reserved.
+// Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+// reference: nuget:WeihanLi.Npoi, 2.4.2
 // using: WeihanLi.Npoi
 
 Console.WriteLine(new[] { 1, 2, 3 }.GetCsvText());
