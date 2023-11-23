0e52ac1f1e33a1633d2b7a38fb6f93b17c26d5a4
Weihan Li <weihanli@outlook.com>
2023-11-23 10:52:38 +0800

    feat: update ReferenceResolver/DependencyInjectionExtensions.cs

---
+++ tests/IntegrationTest/ReferenceResolverExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2022-2023 Weihan Li. All rights reserved.
+// Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+using Microsoft.Extensions.DependencyInjection;
+using ReferenceResolver;
+
+namespace IntegrationTest;
+
+public class ReferenceResolverExtensionsTests(IServiceProvider serviceProvider)
+{
+    [Fact]
+    public void ResolversRegisterTest()
+    {
+        var resolvers = serviceProvider.GetServices<IReferenceResolver>().ToArray();
+        Assert.Equal(5, resolvers.Length);
+    }
+    
+    [Fact]
+    public void NuGetHelperRegisterTest()
+    {
+        Assert.NotNull(serviceProvider.GetService<INuGetHelper>());
+    }
+    
+    [Fact]
+    public void ResolverFactoryRegisterTest()
+    {
+        ArgumentNullException.ThrowIfNull(serviceProvider);
+        Assert.NotNull(serviceProvider.GetService<IReferenceResolverFactory>());
+    }
+
+    public class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddReferenceResolvers();
+        }
+    }
+}
