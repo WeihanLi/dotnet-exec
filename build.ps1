2f0c6ace059659639ccb8ab338ac54e4162ce81c
Weihan Li <weihanli@outlook.com>
2023-12-30 13:34:16 +0800

    build: remove cake build, use dotnet-exec and C# scripts

--- build.ps1
+++ build.ps1
@@ -1,17 +1,9 @@
-[string]$SCRIPT = '.\build.cake'
-
 # set environment variables
 [System.Environment]::SetEnvironmentVariable('CI', 'true')
 
-# Install the latest cake.tool
-dotnet tool update --global cake.tool
 # Install the lastest dotnet-execute tool
-dotnet tool update --global dotnet-execute
+dotnet tool update --global dotnet-execute --prerelease
 
 # Execute CSharp script
-# Write-Host "dotnet-exec ./build/build.cs --args $ARGS" -ForegroundColor GREEN
-dotnet-exec ./build/build.cs --args '--target hello'
-
-# Execute Cake
-Write-Host "dotnet-cake $SCRIPT $ARGS" -ForegroundColor GREEN
-dotnet cake $SCRIPT $ARGS
+Write-Host "dotnet-exec ./build/build.cs --args $ARGS" -ForegroundColor GREEN
+dotnet-exec ./build/build.cs --args "$ARGS"
--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-# Install cake.tool
-dotnet tool install --global cake.tool
 # Install dotnet-exec tool
 dotnet tool update --global dotnet-execute --prerelease
 
+# configure dotnet tool path
 export PATH="$PATH:$HOME/.dotnet/tools"
 
 # Execute CSharp script
 Write-Host "dotnet-exec ./build/build.cs --args $ARGS" -ForegroundColor GREEN
-dotnet-exec ./build/build.cs --args '--target hello'
-
-# Start Cake
-echo "dotnet cake $SCRIPT $@"
-dotnet cake $SCRIPT "$@"
+dotnet-exec ./build/build.cs --args "$ARGS"
