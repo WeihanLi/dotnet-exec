94fdf3a8b307ac8569bff641169c637a1c82d9c3
Weihan Li <weihanli@outlook.com>
2023-12-30 15:31:05 +0800

    build: update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ dotnet tool update --global dotnet-execute --prerelease
 # configure dotnet tool path
 export PATH="$PATH:$HOME/.dotnet/tools"
 
+echo "dotnet-exec ./build/build.cs --args $@"
 dotnet-exec ./build/build.cs --args "$@"
