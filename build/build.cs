fe26f4c947362756b020248e1a38715710b5f81c
Weihan Li <weihanli@outlook.com>
2024-01-02 23:08:03 +0800

    Update build.cs
    specify CliWrap version

--- build/build.cs
+++ build/build.cs
@@ -1,7 +1,7 @@
 // Copyright (c) 2022-2023 Weihan Li. All rights reserved.
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
-// r: "nuget: CliWrap"
+// r: "nuget: CliWrap, 3.6.4"
 
 using CliWrap;
 using Newtonsoft.Json;
