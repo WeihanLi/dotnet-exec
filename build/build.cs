b1b3d3c5ed026e830f941bad535a49b62f2a60c0
Weihan Li <weihanli@outlook.com>
2024-01-04 02:52:16 +0800

    update build.cs

--- build/build.cs
+++ build/build.cs
@@ -1,7 +1,7 @@
 // Copyright (c) 2022-2023 Weihan Li. All rights reserved.
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
-using CliWrap;
+using Newtonsoft.Json;
 
 //
 var target = Guard.NotNull(Argument("target", "Default"));
