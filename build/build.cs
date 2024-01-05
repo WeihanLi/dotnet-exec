445ca0afdafbdc0bc52189974c7975ef033f61bc
Weihan Li <weihanli@outlook.com>
2024-01-05 20:18:44 +0800

    refactor: refactoring build.cs

--- build/build.cs
+++ build/build.cs
@@ -1,17 +1,14 @@
 // Copyright (c) 2022-2023 Weihan Li. All rights reserved.
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
-using Newtonsoft.Json;
-
-//
 var target = Guard.NotNull(Argument("target", "Default"));
 var apiKey = Argument("apiKey", "");
-var stable = ArgumentBool("stable", false);
-var noPush = ArgumentBool("noPush", false);
+var stable = ArgumentBool("stable");
+var noPush = ArgumentBool("noPush");
 
 var solutionPath = "./dotnet-exec.sln";
 string[] srcProjects = ["./src/dotnet-exec/dotnet-exec.csproj", "./src/ReferenceResolver/ReferenceResolver.csproj"];
-string[] testProjects = [ "./tests/UnitTest/UnitTest.csproj", "./tests/IntegrationTest/IntegrationTest.csproj" ];
+string[] testProjects = ["./tests/UnitTest/UnitTest.csproj", "./tests/IntegrationTest/IntegrationTest.csproj"];
 
 await BuildProcess.CreateBuilder()
     .WithSetup(() =>
@@ -26,40 +23,43 @@ await BuildProcess.CreateBuilder()
 
         // dump runtime info
         Console.WriteLine("RuntimeInfo:");
-        Console.WriteLine(ApplicationHelper.RuntimeInfo.ToJson(new JsonSerializerSettings()
+#pragma warning disable CA1869
+        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(ApplicationHelper.RuntimeInfo, new System.Text.Json.JsonSerializerOptions()
         {
-            Formatting = Formatting.Indented
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true
         }));
+#pragma warning restore CA1869
     })
     .WithTask("hello", b => b.WithExecution(() => Console.WriteLine("Hello dotnet-exec build")))
     .WithTask("build", b =>
     {
         b.WithDescription("dotnet build")
-            .WithExecution(() => ExecuteCommandAsync($"dotnet build {solutionPath}"))
+            .WithExecution(cancellationToken => ExecuteCommandAsync($"dotnet build {solutionPath}", cancellationToken))
             ;
     })
     .WithTask("test", b =>
     {
         b.WithDescription("dotnet test")
             .WithDependency("build")
-            .WithExecution(async () =>
+            .WithExecution(async cancellationToken =>
             {
                 foreach (var project in testProjects)
                 {
-                    await ExecuteCommandAsync($"dotnet test {project}");
+                    await ExecuteCommandAsync($"dotnet test {project}", cancellationToken);
                 }
             })
             ;
     })
     .WithTask("pack", b => b.WithDescription("dotnet pack")
         .WithDependency("test")
-        .WithExecution(async () =>
+        .WithExecution(async cancellationToken =>
         {
             if (stable)
             {
                 foreach (var project in srcProjects)
                 {
-                    await ExecuteCommandAsync($"dotnet pack {project} -o ./artifacts/packages");
+                    await ExecuteCommandAsync($"dotnet pack {project} -o ./artifacts/packages", cancellationToken);
                 }
             }
             else
@@ -67,21 +67,22 @@ await BuildProcess.CreateBuilder()
                 var suffix = $"preview-{DateTime.UtcNow:yyyyMMdd-HHmmss}";
                 foreach (var project in srcProjects)
                 {
-                    await ExecuteCommandAsync($"dotnet pack {project} -o ./artifacts/packages --version-suffix {suffix}");
+                    await ExecuteCommandAsync(
+                        $"dotnet pack {project} -o ./artifacts/packages --version-suffix {suffix}", cancellationToken);
                 }
-            }            
+            }
 
             if (noPush)
             {
                 Console.WriteLine("Skip push there's noPush specified");
                 return;
             }
-            
+
             if (string.IsNullOrEmpty(apiKey))
             {
                 // try to get apiKey from environment variable
                 apiKey = Environment.GetEnvironmentVariable("NuGet__ApiKey");
-                
+
                 if (string.IsNullOrEmpty(apiKey))
                 {
                     Console.WriteLine("Skip push since there's no apiKey found");
@@ -92,20 +93,19 @@ await BuildProcess.CreateBuilder()
             // push nuget packages
             foreach (var file in Directory.GetFiles("./artifacts/packages/", "*.nupkg"))
             {
-                await ExecuteCommandAsync($"dotnet nuget push {file} -k {apiKey} --skip-duplicate");
+                await ExecuteCommandAsync($"dotnet nuget push {file} -k {apiKey} --skip-duplicate", cancellationToken);
             }
         }))
     .WithTask("Default", b => b.WithDependency("hello").WithDependency("pack"))
     .Build()
-    .ExecuteAsync(target);
-
+    .ExecuteAsync(target, InvokeHelper.GetExitToken());
 
 bool ArgumentBool(string argumentName, bool defaultValue = default)
 {
     var value = ArgumentInternal(argumentName);
     if (value is null) return defaultValue;
-    if (value == string.Empty || value == "1") return true;
-    return value is "0" ? false : bool.Parse(value);
+    if (value == string.Empty) return true;
+    return bool.Parse(value);
 }
 
 string? Argument(string argumentName, string? defaultValue = default)
@@ -119,28 +119,30 @@ string? ArgumentInternal(string argumentName)
     {
         if (args[i] == $"--{argumentName}" || args[i] == $"-{argumentName}")
         {
-            if (((i + 1) == args.Length || args[i + 1].StartsWith('-')))
+            if (i + 1 == args.Length || args[i + 1].StartsWith('-'))
                 return string.Empty;
 
             return args[i + 1];
         }
 
-        if (args[i].StartsWith($"-{argumentName}="))
-            return args[i].Substring($"-{argumentName}=".Length);
-        
-        if (args[i].StartsWith($"--{argumentName}="))
-            return args[i].Substring($"--{argumentName}=".Length);
+        if (args[i].StartsWith($"-{argumentName}=", StringComparison.Ordinal) 
+            || args[i].StartsWith($"-{argumentName}:", StringComparison.Ordinal))
+            return args[i][$"-{argumentName}=".Length..];
+
+        if (args[i].StartsWith($"--{argumentName}=", StringComparison.Ordinal)
+            || args[i].StartsWith($"--{argumentName}:", StringComparison.Ordinal))
+            return args[i][$"--{argumentName}=".Length..];
     }
 
     return null;
 }
 
-async Task ExecuteCommandAsync(string commandText)
+async Task ExecuteCommandAsync(string commandText, CancellationToken cancellationToken = default)
 {
     Console.WriteLine($"Executing command: \n    {commandText}");
     Console.WriteLine();
 
-    var result = await CommandExecutor.ExecuteCommandAndOutputAsync(commandText);
+    var result = await CommandExecutor.ExecuteCommandAndOutputAsync(commandText, cancellationToken: cancellationToken);
     result.EnsureSuccessExitCode();
     Console.WriteLine();
 }
@@ -151,35 +153,40 @@ file sealed class BuildProcess
     public Func<Task>? Setup { private get; init; }
     public Func<Task>? Cleanup { private get; init; }
 
-    public async Task ExecuteAsync(string target)
+    public async Task ExecuteAsync(string target, CancellationToken cancellationToken = default)
     {
         var task = Tasks.FirstOrDefault(x => x.Name == target);
         if (task is null)
             throw new InvalidOperationException("Invalid target to execute");
-        
+
         try
         {
             if (Setup != null)
                 await Setup.Invoke();
-            
-            await ExecuteTask(task);
+
+            cancellationToken.ThrowIfCancellationRequested();
+            await ExecuteTask(task, cancellationToken);
+        }
+        catch (OperationCanceledException) when (cancellationToken.IsCancellationRequested)
+        {
+            Console.WriteLine("Operation canceled");
         }
         finally
         {
             if (Cleanup != null)
                 await Cleanup.Invoke();
-        }                
+        }
     }
 
-    private static async Task ExecuteTask(BuildTask task)
+    private static async Task ExecuteTask(BuildTask task, CancellationToken cancellationToken)
     {
         foreach (var dependencyTask in task.Dependencies)
         {
-            await ExecuteTask(dependencyTask);
+            await ExecuteTask(dependencyTask, cancellationToken);
         }
 
         Console.WriteLine($"===== Task {task.Name} {task.Description} executing ======");
-        await task.ExecuteAsync();
+        await task.ExecuteAsync(cancellationToken);
         Console.WriteLine($"===== Task {task.Name} {task.Description} executed ======");
     }
 
@@ -197,25 +204,26 @@ file sealed class BuildProcessBuilder
     public BuildProcessBuilder WithTask(string name, Action<BuildTaskBuilder> buildTaskConfigure)
     {
         var buildTaskBuilder = new BuildTaskBuilder(name);
-        buildTaskBuilder.WithTaskFinder(s => _tasks.Find(t => t.Name == s) ?? throw new InvalidOperationException($"No task found with name {s}"));
+        buildTaskBuilder.WithTaskFinder(s =>
+            _tasks.Find(t => t.Name == s) ?? throw new InvalidOperationException($"No task found with name {s}"));
         buildTaskConfigure.Invoke(buildTaskBuilder);
         var task = buildTaskBuilder.Build();
         _tasks.Add(task);
         return this;
     }
-    
+
     public BuildProcessBuilder WithSetup(Action setupFunc)
     {
         _setup = setupFunc.WrapTask();
         return this;
     }
-    
+
     public BuildProcessBuilder WithSetup(Func<Task> setupFunc)
     {
         _setup = setupFunc;
         return this;
     }
-    
+
     public BuildProcessBuilder WithCleanup(Action cleanupFunc)
     {
         _cleanup = cleanupFunc.WrapTask();
@@ -230,23 +238,19 @@ file sealed class BuildProcessBuilder
 
     internal BuildProcess Build()
     {
-        return new BuildProcess()
-        {
-            Tasks = _tasks,
-            Setup = _setup,
-            Cleanup = _cleanup
-        };
+        return new BuildProcess() { Tasks = _tasks, Setup = _setup, Cleanup = _cleanup };
     }
 }
 
-file sealed class BuildTask(string name, string? description, Func<Task>? execution = null)
+file sealed class BuildTask(string name, string? description, Func<CancellationToken, Task>? execution = null)
 {
     public string Name => name;
     public string Description => description ?? name;
 
     public IReadOnlyCollection<BuildTask> Dependencies { get; init; } = [];
 
-    public Task ExecuteAsync() => execution?.Invoke() ?? Task.CompletedTask;
+    public Task ExecuteAsync(CancellationToken cancellationToken = default) =>
+        execution?.Invoke(cancellationToken) ?? Task.CompletedTask;
 }
 
 file sealed class BuildTaskBuilder(string name)
@@ -254,7 +258,7 @@ file sealed class BuildTaskBuilder(string name)
     private readonly string _name = name;
 
     private string? _description;
-    private Func<Task>? _execution;
+    private Func<CancellationToken, Task>? _execution;
     private readonly List<BuildTask> _dependencies = [];
 
     public BuildTaskBuilder WithDescription(string description)
@@ -262,39 +266,44 @@ file sealed class BuildTaskBuilder(string name)
         _description = description;
         return this;
     }
-    
+
     public BuildTaskBuilder WithExecution(Action execution)
     {
-        _execution = execution.WrapTask();
+        _execution = execution.WrapTask().WrapCancellation();
         return this;
     }
+
     public BuildTaskBuilder WithExecution(Func<Task> execution)
+    {
+        _execution = execution.WrapCancellation();
+        return this;
+    }
+
+    public BuildTaskBuilder WithExecution(Func<CancellationToken, Task> execution)
     {
         _execution = execution;
         return this;
     }
-    
+
     public BuildTaskBuilder WithDependency(string dependencyTaskName)
     {
         if (_taskFinder is null) throw new InvalidOperationException("Dependency task name is not supported");
-        
+
         _dependencies.Add(_taskFinder.Invoke(dependencyTaskName));
         return this;
     }
 
     private Func<string, BuildTask>? _taskFinder;
+
     internal BuildTaskBuilder WithTaskFinder(Func<string, BuildTask> taskFinder)
     {
         _taskFinder = taskFinder;
         return this;
     }
-    
+
     public BuildTask Build()
     {
-        var buildTask = new BuildTask(_name, _description, _execution)
-        {
-            Dependencies = _dependencies
-        };
+        var buildTask = new BuildTask(_name, _description, _execution) { Dependencies = _dependencies };
         return buildTask;
     }
 }
