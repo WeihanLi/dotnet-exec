bd95ab6e976e83064aff1954f0efd873b3a936bc
Weihan Li <weihanli@outlook.com>
2024-01-06 15:43:15 +0800

    Update build.cs
    remove test code
    
    [skip ci]

--- build/build.cs
+++ build/build.cs
@@ -1,8 +1,6 @@
 // Copyright (c) 2022-2024 Weihan Li. All rights reserved.
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
-// if (args.IsNullOrEmpty()) args = ["--target", "build"];
-
 var target = Guard.NotNull(Argument("target", "Default"));
 var apiKey = Argument("apiKey", "");
 var stable = ArgumentBool("stable");
