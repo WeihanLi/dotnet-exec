88da68755dff0e8ad06b83066d3365c8c4672ca4
Weihan Li <weihanli@outlook.com>
2024-06-13 08:27:23 +0800

    build: update build script

--- build.ps1
+++ build.ps1
@@ -2,9 +2,9 @@
 [System.Environment]::SetEnvironmentVariable('CI', 'true')
 
 dotnet build -c Release -p VersionSuffix=dev ./src/dotnet-exec/dotnet-exec.csproj -f net9.0 -o ./artifacts/out/build
-Set-Alias -Name d-exe -Value ./artifacts/out/build/dotnet-exec.exe
+Set-Alias -Name d_exe -Value ./artifacts/out/build/dotnet-exec.exe
 
-d-exe --info
+d_exe --info
 
 # Execute CSharp script
-d-exe ./build/build.cs --args "$ARGS"
+d_exe ./build/build.cs --args "$ARGS"
--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 dotnet build -c Release -p VersionSuffix=dev ./src/dotnet-exec/dotnet-exec.csproj -f net9.0 -o ./artifacts/out/build
 
-alias d-exe="./artifacts/out/build/dotnet-exec"
+export d_exe="./artifacts/out/build/dotnet-exec"
 
-d-exe --info
+$d_exe --info
 
 echo "dotnet-exec ./build/build.cs --args $@"
-d-exe ./build/build.cs --args "$@"
+$d_exe ./build/build.cs --args "$@"
