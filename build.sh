06f606353f6a40d1e699aad42b3a8aac64a1f412
Weihan Li <7604648+WeihanLi@users.noreply.github.com>
2024-09-09 08:20:20 +0800

    build: Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 dotnet build -c Release -p VersionSuffix=dev ./src/dotnet-exec/dotnet-exec.csproj -f net9.0 -o ./artifacts/out/build
 
-export d_exe="./artifacts/out/build/dotnet-exec"
+dotnet_exec="./artifacts/out/build/dotnet-exec"
 
-$d_exe --info
+$dotnet_exec --info
 
 echo "dotnet-exec ./build/build.cs --args $@"
-$d_exe ./build/build.cs --args "$@"
+$dotnet_exec ./build/build.cs --args "$@"
