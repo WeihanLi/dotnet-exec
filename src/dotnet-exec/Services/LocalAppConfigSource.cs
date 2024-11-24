219dec320af6e86da85edba527cb87393fc84f3b
Weihan Li <weihanli@outlook.com>
2024-11-24 10:48:42 +0800

    refactor: add ExcludeFromCodeCoverage

--- src/dotnet-exec/Services/LocalAppConfigSource.cs
+++ src/dotnet-exec/Services/LocalAppConfigSource.cs
@@ -2,9 +2,12 @@
 // Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
 
 using Exec.Contracts;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 
 namespace Exec.Services;
+
+[ExcludeFromCodeCoverage]
 internal sealed class LocalAppConfigSource : IAppConfigSource
 {
     private static readonly string DefaultConfigPath = Path.Combine(
