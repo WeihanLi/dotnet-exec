06298d3234906a29580c9ba5355624d05d8d3de7
Weihan Li <weihanli@outlook.com>
2025-09-21 12:16:57 +0800

    feat: init dotnet-exec-mcp

---
+++ src/dotnet-exec-mcp/ExecTool.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2022-2025 Weihan Li. All rights reserved.
+// Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+using Exec;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace ExecMcp;
+
+public class ToolExecOptions
+{
+    public required string Script { get; set; }
+
+    [Description("The package references for the execution")]
+    public string[]? References { get; set; }
+    
+    [Description("The namespace using for the execution")]
+    public string[]? Usings { get; set; }
+}
+
+[McpServerToolType]
+public class ExecTool(CommandHandler commandHandler)
+{
+    public async Task<int> Exec(ToolExecOptions options)
+    {
+        var execOptions = new ExecOptions
+        {
+            Script = options.Script
+        };
+        return await commandHandler.Execute(execOptions);
+    }
+}
---
+++ src/dotnet-exec-mcp/Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2022-2025 Weihan Li. All rights reserved.
+// Licensed under the Apache license version 2.0 http://www.apache.org/licenses/LICENSE-2.0
+
+using ExecMcp;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<ExecTool>()
+    ;
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+await builder.Build().RunAsync();
