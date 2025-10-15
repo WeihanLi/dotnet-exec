0cafdf735e3f337fd88b277c9728ac38c33a59c2
Weihan Li <weihanli@outlook.com>
2025-10-15 20:00:56 +0800

    feat: update exec tool

--- src/dotnet-exec-mcp/ExecTool.cs
+++ src/dotnet-exec-mcp/ExecTool.cs
@@ -7,26 +7,30 @@ using System.ComponentModel;
 
 namespace ExecMcp;
 
-public class ToolExecOptions
-{
-    public required string Script { get; set; }
-
-    [Description("The package references for the execution")]
-    public string[]? References { get; set; }
-    
-    [Description("The namespace using for the execution")]
-    public string[]? Usings { get; set; }
-}
-
 [McpServerToolType]
 public class ExecTool(CommandHandler commandHandler)
 {
-    public async Task<int> Exec(ToolExecOptions options)
+    [McpServerTool(Name = "execute-C#-script", Title = "Execute C# script, either script text or script file path")]
+    public async Task<int> Exec(
+        [Description("The C# script text or file path to execute")] string script,
+        [Description("The package references for the execution, reference format: `WeihanLi.Npoi@2.4.2`")] string[]? references,
+        [Description("The namespace using for the execution, format: `WeihanLi.Npoi` or `static System.Console`")] string[]? usings
+        )
     {
         var execOptions = new ExecOptions
         {
-            Script = options.Script
+            Script = script
         };
+
+        foreach (var reference in references ?? [])
+        {
+            execOptions.References.Add("nuget: " + reference);
+        }
+        foreach (var @using in usings ?? [])
+        {
+            execOptions.Usings.Add(@using);
+        }
+
         return await commandHandler.Execute(execOptions);
     }
 }
